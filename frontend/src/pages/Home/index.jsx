45886d1d4918faa49b6a57c35509235373626fbb
LucianDeveloper <misha31rudland@gmail.com>
2021-12-18 13:41:40 +0300

    [ADD] A lot of something

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import drop from "../../media/img/drop.svg";
 import edit from "../../media/img/edit.svg";
 import bin from "../../media/img/bin.svg";
-import telega from "../../media/img/telega.svg";
+import news_img from "../../media/img/news_img.jpg";
 
 
 const Home = () => {
@@ -23,15 +23,33 @@ const Home = () => {
                         <div className="news_item">
                             <p className="news_title">Не облагаемый налогом доход по вкладам россиян увеличится до 85 тысяч рублей в 2022 году</p>
                             <p className="news_subtitle">Жители России, имеющие вклады в банках в общей сумме более 1 миллиона рублей, у которых процентный доход превышает 85 тысяч рублей...</p>
+                            <div className="news_img_wrap"><img  className="news_img" src={news_img} alt=""/></div>
                         </div>
+                        <div className="news_item">
+                            <p className="news_title">Не облагаемый налогом доход по вкладам россиян увеличится до 85 тысяч рублей в 2022 году</p>
+                            <p className="news_subtitle">Жители России, имеющие вклады в банках в общей сумме более 1 миллиона рублей, у которых процентный доход превышает 85 тысяч рублей...</p>
+                            <div className="news_img_wrap"><img  className="news_img" src={news_img} alt=""/></div>
+                        </div>
+                        <div className="news_item">
+                            <p className="news_title">Не облагаемый налогом доход по вкладам россиян увеличится до 85 тысяч рублей в 2022 году</p>
+                            <p className="news_subtitle">Жители России, имеющие вклады в банках в общей сумме более 1 миллиона рублей, у которых процентный доход превышает 85 тысяч рублей...</p>
+                            <div className="news_img_wrap"><img  className="news_img" src={news_img} alt=""/></div>
+                        </div>
+                        <div className="news_item">
+                            <p className="news_title">Не облагаемый налогом доход по вкладам россиян увеличится до 85 тысяч рублей в 2022 году</p>
+                            <p className="news_subtitle">Жители России, имеющие вклады в банках в общей сумме более 1 миллиона рублей, у которых процентный доход превышает 85 тысяч рублей...</p>
+                            <div className="news_img_wrap"><img  className="news_img" src={news_img} alt=""/></div>
+                        </div>
+
                     </div>
+
                 </div>
                 <div className="clients_side">
                     <div className="clients_side_card">
                         <p className="clients_side_card_title">Вид публикации</p>
-                        <a href=""  className="clients_side_card_btn_blue"> <img  className="telega_img" alt=""/>Новости</a>
-                        <a href=""  className="clients_side_card_btn_blue"> <img  className="telega_img" alt=""/>Статьи</a>
-                        <a href=""  className="clients_side_card_btn_blue"> <img  className="telega_img" alt=""/>Документы</a>
+                        <a href=""  className="news_side_card_btn_blue">Новости</a>
+                        <a href=""  className="news_side_card_btn_blue">Статьи</a>
+                        <a href=""  className="news_side_card_btn_blue">Документы</a>
                     </div>
                 </div>
             </div>
